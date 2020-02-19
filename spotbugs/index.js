a6e75b9f6c1fb175d59c6bccd5c4384b4b3d0abe
Marc Streckfuß <marc@fam-streckfuss.de>
2020-02-19 21:29:23 +0100

    Try to parse XML

--- spotbugs/index.js
+++ spotbugs/index.js
@@ -1,5 +1,6 @@
 const exec = require('child-process-promise').exec;
 const glob = require('glob-promise');
+const xml2js = require('xml2js');
 
 async function gradlew(task) {
     return exec("cd ../../ && java -cp gradle/wrapper/gradle-wrapper.jar org.gradle.wrapper.GradleWrapperMain " + task)
@@ -8,9 +9,15 @@ async function gradlew(task) {
 async function main() {
 //await exec("pwd").then(out => console.log(out.stdout));
 await gradlew("spotbugsMain -Pxml-reports=true --continue").then(out => console.log(out.stderr)).catch(err => { console.error(err) });
-glob('../../**/build/reports/spotbugs/main.xml').then(files => { 
-    files.forEach(file => console.log(file));
-});
+glob('../../**/build/reports/spotbugs/main.xml').then(files => 
+    files.forEach(file => {
+        console.log(file);
+        xml2js.parseStringPromise(file /*, options */).then((result) => {
+            console.dir(result);
+            console.log('Done');
+          });
+    })
+);
 // jme3-core/build/reports/spotbugs/
 }
 
