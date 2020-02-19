9d72780e8f9a2d16147d8520777ebeab90a7d6d1
Marc Streckfuß <marc@fam-streckfuss.de>
2020-02-19 17:09:02 +0100

    Try to fix the gradle wrapper invocation

--- spotbugs/index.js
+++ spotbugs/index.js
@@ -1,9 +1,10 @@
 const exec = require('child-process-promise').exec;
 
 async function gradlew(task) {
-    return exec("java -cp gradle-wrapper.jar org.gradle.wrapper.GradleWrapperMain " + task)
+    return exec("java -cp gradle/wrapper/gradle-wrapper.jar org.gradle.wrapper.GradleWrapperMain " + task)
 }
 
-await gradlew("spotbugsMain -Pxml-reports=true").then(out => console.log(out.stderr)).catch(err => console.error(err))
-
-// jme3-core/build/reports/...
+console.log("Pre gradlew");
+await gradlew("spotbugsMain -Pxml-reports=true").then(out => console.log(out.stderr)).catch(err => { console.log(err); console.error(err) });
+console.log("post gradlew");
+// jme3-core/build/reports/tests/
