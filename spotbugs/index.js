d762bdb6a84263dbe16432b87636d016b3745ffb
Marc Streckfuß <marc@fam-streckfuss.de>
2020-02-19 21:10:42 +0100

    Added glob and glob-promise and also fetch all main.xml files as a list

--- spotbugs/index.js
+++ spotbugs/index.js
@@ -1,14 +1,15 @@
 const exec = require('child-process-promise').exec;
+const glob = require('glob-promise');
 
 async function gradlew(task) {
     return exec("cd ../../ && java -cp gradle/wrapper/gradle-wrapper.jar org.gradle.wrapper.GradleWrapperMain " + task)
 }
 
 async function main() {
-await exec("pwd").then(out => console.log(out.stdout));
+//await exec("pwd").then(out => console.log(out.stdout));
 await gradlew("spotbugsMain -Pxml-reports=true --continue").then(out => console.log(out.stderr)).catch(err => { console.error(err) });
-await exec("pwd").then(out => console.log(out));
-// jme3-core/build/reports/tests/
+glob('../../**/build/reports/spotbugs/main.xml').then(files => console.log(files));
+// jme3-core/build/reports/spotbugs/
 }
 
 try {
@@ -16,4 +17,3 @@ try {
 } catch (e) {
     console.error(e);
 }
-
