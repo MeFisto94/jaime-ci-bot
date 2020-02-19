862e75209a359de479a8b6864140f688e9d41709
Marc Streckfuß <marc@fam-streckfuss.de>
2020-02-19 18:01:42 +0100

    More debugging output and chdir

--- spotbugs/index.js
+++ spotbugs/index.js
@@ -1,11 +1,13 @@
 const exec = require('child-process-promise').exec;
 
 async function gradlew(task) {
-    return exec("java -cp gradle/wrapper/gradle-wrapper.jar org.gradle.wrapper.GradleWrapperMain " + task)
+    return exec("cd ../../ && java -cp gradle/wrapper/gradle-wrapper.jar org.gradle.wrapper.GradleWrapperMain " + task)
 }
 
 async function main() {
-await gradlew("spotbugsMain -Pxml-reports=true").then(out => console.log(out.stderr)).catch(err => { console.log(err); console.error(err) });
+await exec("pwd").then(out => console.log(out.stdout));
+await gradlew("spotbugsMain -Pxml-reports=true").then(out => console.log(out.stderr)).catch(err => { console.error(err) });
+await exec("pwd").then(out => console.log(out));
 // jme3-core/build/reports/tests/
 }
 
