cb91c983adf627574421732a4c8ece1234a20941
Marc Streckfuß <marc@fam-streckfuss.de>
2020-02-19 17:39:41 +0100

    add "start" script, use npm start

--- spotbugs/index.js
+++ spotbugs/index.js
@@ -4,7 +4,14 @@ async function gradlew(task) {
     return exec("java -cp gradle/wrapper/gradle-wrapper.jar org.gradle.wrapper.GradleWrapperMain " + task)
 }
 
-console.log("Pre gradlew");
+async function main() {
 await gradlew("spotbugsMain -Pxml-reports=true").then(out => console.log(out.stderr)).catch(err => { console.log(err); console.error(err) });
-console.log("post gradlew");
 // jme3-core/build/reports/tests/
+}
+
+try {
+    main();
+} catch (e) {
+    console.error(e);
+}
+
